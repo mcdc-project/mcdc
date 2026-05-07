bc24541ed35cbf34fd40fd56f7b70e1ca144d4b8
Nathan Glaser <glaserna@oregonstate.edu>
2026-05-06 20:03:26 -0700

    added polar reference vector to ww object

--- mcdc/object_/technique.py
+++ mcdc/object_/technique.py
@@ -99,6 +99,11 @@ class WeightWindows(ObjectSingleton):
     Ny: int
     Nz: int
 
+    # reference vector for calculating mu and azimuthal angle
+    px: float
+    py: float
+    pz: float
+
     # arrays of ww params
     lower_weights: Annotated[
         NDArray[np.float64], ("Nt", "Ne", "Nmu", "Na", "Nx", "Ny", "Nz")
@@ -123,6 +128,9 @@ class WeightWindows(ObjectSingleton):
         self.mesh_ID = -1  # skirt around having to create a MeshBase instance
         self.Nx, self.Ny, self.Nz = 1, 1, 1
         self.Nt = 1
+        self.px = 0.0
+        self.py = 0.0
+        self.pz = 1.0
         shape = (self.Nt, self.Ne, self.Nmu, self.Na, self.Nx, self.Ny, self.Nz)
         self.lower_weights = np.array([1.0]).reshape(*shape)
         self.target_weights = np.array([1.0]).reshape(*shape)
@@ -207,6 +215,12 @@ class WeightWindows(ObjectSingleton):
             print_error(
                 "Target weight can not be greater than the upper bound weight for any weight window"
             )
+
+    def set_polar_reference(self, px, py, pz):
+        norm = np.linalg.norm([px, py, pz])
+        self.px = px / norm
+        self.py = py / norm
+        self.pz = pz / norm
 
     @staticmethod
     def __check_array(array: NDArray[np.float64], name: str):
