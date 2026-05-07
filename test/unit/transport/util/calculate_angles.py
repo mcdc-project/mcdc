dd78e013d6de70d6203555804a1642efe04df9d3
Nathan Glaser <glaserna@oregonstate.edu>
2026-05-06 20:27:35 -0700

    add quick unit test for angle helper

---
+++ test/unit/transport/util/calculate_angles.py
@@ -0,0 +1,31 @@
+import numpy as np
+import pytest
+
+from mcdc.transport.util import calculate_angles
+from mcdc.numba_types import particle_data
+
+
+@pytest.mark.parametrize(
+    "ux, uy, uz, expected_mu, expected_phi",
+    [
+        (1.0, 0.0, 0.0, 0.0, 0.0),
+        (-1.0, 0.0, 0.0, 0.0, np.pi),
+        (0.0, 1.0, 0.0, 0.0, np.pi / 2.0),
+        (0.0, -1.0, 0.0, 0.0, -np.pi / 2.0),
+        (0.0, 0.0, 1.0, 1.0, 0.0),
+        (0.0, 0.0, -1.0, -1.0, 0.0)
+    ],
+)
+def test_calculate_angles(ux, uy, uz, expected_mu, expected_phi):
+    particle_container = np.zeros(1, particle_data)
+    particle = particle_container[0]
+
+    particle["ux"] = ux
+    particle["uy"] = uy
+    particle["uz"] = uz
+    px, py, pz = 0.0, 0.0, 1.0
+
+    mu, phi = calculate_angles(particle_container, px, py, pz)
+
+    assert np.isclose(mu, expected_mu)
+    assert np.isclose(phi, expected_phi)
