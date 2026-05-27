035e1a4ea4aa064a34fdf2e0a94fb35dfc09b043
Ethan Lame <ethan0824@gmail.com>
2026-05-27 14:54:39 -0700

    added CSDA setting to input deck

--- mcdc/numba_types.py
+++ mcdc/numba_types.py
@@ -595,6 +595,7 @@ settings = into_dtype([
     ('time_boundary', float64),
     ('output_name', 'U32'),
     ('use_progress_bar', bool),
+    ('csda', bool),
     ('N_census', int64),
     ('census_time_offset', int64),
     ('census_time_length', int64),
